9dfdb1c72d94b790ffd75dd0bec6997342f97b19
Mario Marković <mmarkovi3g@gmail.com>
2023-07-12 10:39:02 +0200

    added filter for years

--- src/App.js
+++ src/App.js
@@ -1,34 +1,44 @@
 import ExpenseItem from "./components/Expenses/ExpenseItem";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
+import { useState } from "react";
+
+const DUMMY_EXPENSES = [
+  //ovdje je u listu dodano nekoliko objekata s različitim svojstvima
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+  {
+    id: "e5",
+    title: "Automobil",
+    amount: 6698,
+    date: new Date(2023, 5, 12),
+  },
+];
 
 const App = () => {
-  const expenses = [
-    //ovdje je u listu dodano nekoliko objekata s različitim svojstvima
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log("In APP.js");
-    console.log(expense);
+    setExpenses((previousExpense) => {
+      return [expense, ...previousExpense];
+    });
   };
 
   return (
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,20 +4,15 @@ import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-  //react komponente uvijek uzimaju samo jedan argument - najčešće se zove PROPS
+  //react komponente uvijek uzimaju samo jedan argument - props
+
   const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle("Evo ovo radi!");
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">$ {props.amount}</div>
-        <button onClick={clickHandler}>Change title</button>
       </div>
     </Card>
   );
